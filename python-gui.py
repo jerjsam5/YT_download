5851c13981e81c3a8b75905e88f286354e5ea377
jerjsam5 <jsamuels55@hotmail.co.uk>
2021-09-27 21:56:06 +0100

    messing with tkinter some more

--- python-gui.py
+++ python-gui.py
@@ -1,8 +1,16 @@
 import tkinter as tk
 window = tk.Tk()
 
-
-greeting = tk.Label(text="YouTube to MP4 Downloader")
+greeting = tk.Label(
+    text = "YouTube to MP4 Downloader",
+    fg   = "black",
+    bg   = "red")
 greeting.pack()
+
+entry = tk.Entry()
+entry.pack()
+path = entry.get
+
+
 
 window.mainloop()
