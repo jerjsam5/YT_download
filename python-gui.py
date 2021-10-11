15ac28e29f493708fd50c305d0a36563226056d0
jerjsam5 <jsamuels55@hotmail.co.uk>
2021-10-11 21:06:14 +0100

    couple changes, nothing important

--- python-gui.py
+++ python-gui.py
@@ -1,15 +1,13 @@
 import tkinter as tk
 window = tk.Tk()
 
+frame = tk.Frame()
+
 greeting = tk.Label(
     text = "YouTube to MP4 Downloader",
     fg   = "black",
     bg   = "red")
 greeting.pack()
-
-entry = tk.Entry()
-entry.pack()
-path = entry.get
 
 
 
